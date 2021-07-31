a85a6c8fd5c72fa2c4e05a6bdbfff3ae52a2eb84
Bohun Tak <bhtak@uangel.com>
2021-07-31 15:49:48 +0900

    add

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,10 @@
-/// <reference types="react" />
-declare const LibraryName: () => JSX.Element;
-export default LibraryName;
+import React from "react";
+import { StyleProp, TextStyle } from "react-native";
+declare type StyledTextProps = {
+    text: string;
+    style: StyleProp<TextStyle>;
+    textStyle?: Record<string, StyleProp<TextStyle>>;
+};
+declare const _default: React.NamedExoticComponent<StyledTextProps>;
+export default _default;
 //# sourceMappingURL=index.d.ts.map
